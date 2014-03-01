f7b6ba17f550c2aa16c416e7bf4920c39e7d1512
NadiaNV <nadianavarrov@gmail.com>
2014-03-01 13:26:11 +0100

    Create Anatomia.java

---
+++ Anatomia.java
@@ -0,0 +1,15 @@
+/*02 - VideotutorialJava
+http://www.youtube.com/watch?v=dvI_Fdflzi4&list=PLUeHzPBdeXoG6yFDA79d-1wyr4Pri1GhN
+*/
+
+//Declarar una clase (el nombre de la clase coincide con el nombre de archivo)
+class Anatomia {
+    //Declaración (statement) de main (punto de partida del programa)
+    public static void main(String[]args) {
+        //Mi primer programa Java va aquí
+        //almacena en la variable saludo del tipo string el texto entre ""
+        String saludo ="¡Bienvenido al curso Java!";
+        //Muestra por pantalla el contenido de la variable saludo
+        System.out.println(saludo);
+    }
+} 
