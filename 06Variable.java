892ad66a050a296f6b8e097aa77bf676c4378497
NadiaNV <nadianavarrov@gmail.com>
2014-03-01 13:31:37 +0100

    Create 06Variable.java

---
+++ 06Variable.java
@@ -0,0 +1,26 @@
+/* 04 - VideotutorialJava
+http://www.youtube.com/watch?v=brOp2KOZ9VM&list=PLUeHzPBdeXoG6yFDA79d-1wyr4Pri1GhN*/
+
+//Programa llamado variable
+class Variable {
+	//cuenta con un bloque llamado main    
+	public static void main (String[] args) {
+		//creacion de variable del tipo int (numero entero) llamada cantidad
+		int cantidad;
+		//variable del tipo float (numeros decimales)
+		float puntuacionPromedio;
+		//variable del tipo char (caracter) entre comillas simples
+		char clave = 'C';
+		//variable del tipo String (cadena) entre comillas dobles
+		//Si no estuviese entre comillas significa que es el valor de otra varialble
+		String nombreAlumno = "Anael";
+		//variable del tipo boolean (condicional: true o false)
+		boolean videoFinalizado = false;
+		
+		//Creación de Constantes (son variables pero el valor no cambia)
+		//se añade la palabra final delante del tipo de la constante
+		//el nombre debe ponerse en mayuscula
+		final int PRIMA = 12000;
+		final double PI = 3.1416;
+	}
+}
